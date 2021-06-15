222334a7bc09f683039ca8c429335da3d781889f
flaviopessini <flavio.pessini@outlook.com>
2021-06-15 17:11:01 -0300

    Criação dos recursos do módulo de Produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.status(200).json({ message: 'Hello World' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
