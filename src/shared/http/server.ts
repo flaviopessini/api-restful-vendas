01a8c3d13512be018044875d7ad261681a9b6774
flaviopessini <flavio.pessini@outlook.com>
2021-06-15 17:23:45 -0300

    Validação das rotas de Produto com Celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes/index';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 // Middleware captura e trata os erros.
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
