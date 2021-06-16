6f3018af39ffc9eda68bacb399bb674fc62a1f4b
flaviopessini <flavio.pessini@outlook.com>
2021-06-16 14:48:57 -0300

    Finalizada implementação de upload de avatar do usuário

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,16 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 import UserValidations from '../utils/UserValidations';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const validations = new UserValidations();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 usersRouter.get('/:id', validations.showValidation, usersController.show);
 usersRouter.post('/', validations.createValidation, usersController.create);
 // TODO: update
+usersRouter.patch(
+  '/avatar/',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
 usersRouter.delete(
   '/:id',
   validations.deleteValidation,
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,12 +5,14 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes/index';
 import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
 import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
 // Middleware captura e trata os erros.
