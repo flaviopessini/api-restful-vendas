f51370d29f70ce1605621a7cab843842d59e8555
flaviopessini <flavio.pessini@outlook.com>
2021-06-16 10:44:15 -0300

    Implementado criptografia de senha no usuário

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
@@ -19,10 +20,11 @@ class CreateUserService {
         400,
       );
     }
+    const hashPasswd = await hash(password, 8);
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashPasswd,
     });
     await userRepository.save(user);
     return user;
