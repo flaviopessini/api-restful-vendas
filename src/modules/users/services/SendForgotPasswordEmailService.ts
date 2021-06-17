8324ba33f8f11d3843cb715a3995cc7e581136a1
flaviopessini <flavio.pessini@outlook.com>
2021-06-17 10:55:42 -0300

    Implenetação do Ethereal Fake Email Service

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -22,7 +23,11 @@ class SendForgotPasswordEmailService {
         500,
       );
     }
-    console.log('TOKEN => ', token);
+    // console.log('TOKEN => ', token);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token.token}`,
+    });
   }
 }
 
