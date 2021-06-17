965eccbb283b03bc0daefd54b5e64171f479d66b
flaviopessini <flavio.pessini@outlook.com>
2021-06-17 13:14:31 -0300

    Gerenciamento de perfil do usuário

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findOne({ id: user_id });
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
+    }
+    return user;
+  }
+}
+
+export default ShowProfileService;
--- src/modules/users/utils/UserValidations.ts
+++ src/modules/users/utils/UserValidations.ts
@@ -52,4 +52,19 @@ export default class UserValidations {
       password_confirmation: Joi.string().required().valid(Joi.ref('password')),
     },
   });
+
+  updateProfileValidation = celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().optional(),
+      password_confirmation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  });
 }
