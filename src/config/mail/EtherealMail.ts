d50fdb4e4ec0238ba1a146506c31ddaf0d97f924
flaviopessini <flavio.pessini@outlook.com>
2021-06-17 12:29:45 -0300

    Conclusão do envio de email para redefinição de senha

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,12 +1,35 @@
 import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ITemplateVariable {
+  [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+  file: string;
+  variables: ITemplateVariable;
+}
 
 interface ISendMail {
-  to: string;
-  body: string;
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
 }
 
 export default class EtherealMail {
-  static async sendMail({ to, body }: ISendMail): Promise<void> {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const mailTemplate = new HandlebarsMailTemplate();
     const account = await nodemailer.createTestAccount();
     const transporter = nodemailer.createTransport({
       host: account.smtp.host,
@@ -18,10 +41,16 @@ export default class EtherealMail {
       },
     });
     const message = await transporter.sendMail({
-      from: 'equipe@apivendas.com.br',
-      to: to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe API Vendas',
+        address: from?.email || 'equipe@apivendas.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parser(templateData),
     });
     console.log('Message sent: %s', message.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import path from 'path';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
@@ -9,6 +10,9 @@ interface IRequest {
 }
 
 class SendForgotPasswordEmailService {
+  private readonly frontendResetPasswordURL =
+    'http://localhost:3000/reset_password?token=';
+
   public async execute({ email }: IRequest): Promise<void> {
     const userRepository = getCustomRepository(UsersRepository);
     const userTokensRepository = getCustomRepository(UserTokensRepository);
@@ -16,17 +20,32 @@ class SendForgotPasswordEmailService {
     if (!user) {
       throw new AppError('Usuário não existe');
     }
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
     if (!token) {
       throw new AppError(
         'Houve uma falha ao gerar token para resetar senha',
         500,
       );
     }
-    // console.log('TOKEN => ', token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `${this.frontendResetPasswordURL}${token}`,
+        },
+      },
     });
   }
 }
