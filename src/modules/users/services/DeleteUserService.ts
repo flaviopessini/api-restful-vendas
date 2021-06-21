e5262ae70896405daef94654b31b6a8470aa0ad8
flaviopessini <flavio.pessini@outlook.com>
2021-06-21 19:43:43 -0300

    Correção DelteUser

--- src/modules/users/services/DeleteUserService.ts
+++ src/modules/users/services/DeleteUserService.ts
@@ -1,19 +1,24 @@
 import AppError from '@shared/errors/AppError';
-import { getCustomRepository } from 'typeorm';
-import { UsersRepository } from '../infra/typeorm/repositories/UsersRepository';
+import { injectable, inject } from 'tsyringe';
+import { IUsersRepository } from '../domain/repositories/IUsersRepository';
 
 interface IRequest {
   id: string;
 }
 
+@injectable()
 class DeleteUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
   public async execute({ id }: IRequest): Promise<void> {
-    const usersRepository = getCustomRepository(UsersRepository);
-    const user = await usersRepository.findOne(id);
+    const user = await this.usersRepository.findById(id);
     if (!user) {
       throw new AppError('Usuário não encontrado', 400);
     }
-    await usersRepository.remove(user);
+    // await this.usersRepository.remove(user);
   }
 }
 
